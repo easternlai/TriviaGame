e59fa33edd60bad19f884b24546791b4bcb9e025
Eastern Lai <easternlai@hotmail.com>
2019-10-27 10:50:46 -0700

    added notes to code, cleaned up extra lines

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # TriviaGame
+
+##Tecnologies Used
+
+1. HTML
+2. CSS
+3. Github
+4. visual studio code
+5. Git Bash
+6. JQuery
+7. JavaScript 
+8. Bootstrap
+
+
+Code Sample
+
+```
+
+$(".selection").on("click", function(){
+    that = this;
+
+    if(answerInterval === true){
+        answerInterval = false;
+        resetTimeout();
+        if ($(this).text() === currentQuestion.answer.answer){
+            userCorrect();
+        }else{
+            userIncorrect();
+        }
+
+        updateScore();
+
+        checkGameEnd();
+
+        setTimeout(newQuestion,5000);
+    }
+
+
+});
+```
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,28 @@
 var QnA = [
     {
-        question: {q:"This character was an only child."},
-        answer: {a: "Rachel", b: "Joey", c: "Chandler", d:"Pheobe", answer: "Chandler"}
+        question: {q:"How Many sisters does Joey have?"},
+        answer: {a: "Five", b: "Six", c: "Seven", d:"Eight", answer: "Seven"}
     }, 
     {
-        question: {q:"Molly Ringwald was originally offered the role of this character."},
-        answer: {a: "Monica", b: "Pheobe", c: "Rachel", d:"Janice", answer: "Monica"}
+        question: {q:"The role of Monica Geller was originally offered to this actress?"},
+        answer: {a: "Demi Moore", b: "Sarah Jessica Parkere", c: "Brooke Shields", d:"Molly Ringwald",answer: "Molly Ringwald"}
     }, 
     {
-        question: {q:"This Character was last to join the group."},
-        answer: {a: "Rachel", b: "Joey", c: "Chandler", d:"Pheobe", answer: "Joey"}
+        question: {q:"What was the name of the self help book that the girls loved?"},
+        answer: {a: "Be Your Own Cleansing Pool", b: "Be Your Own Person", c: "Be Your Own Lightning Bearer", d:"Be Your Own Windkeeper", answer: "Be Your Own Windkeeper"}
     }, 
     {
-        question: {q:"This is the oldest character."},
-        answer: {a: "Pheobe", b: "Joey", c: "Chandler", d:"Ross", answer: "Pheobe"}
+        question: {q:"Which character was the oldest?"},
+        answer: {a: "Pheobe", b: "Joey", c: "Chandler", d:"Ross", answer: "Ross"}
     }, 
     {
-        question: {q:"This character was the first to ever live in Monica's apartment."},
-        answer: {a: "Joey", b: "Monica", c: "Ross", d:"Pheobe", answer: "Ross"}
+        question: {q:"What is Chandler's job?"},
+        answer: {a: "IT Procurement Manager", b: "Financial Systems Analyst", c: "Financial Planner", d:"Payroll Accountant", answer: "IT Procurement Manager"}
     }, 
     {
-        question: {q:"This friends cast member also starred in the film The Leprechaun."},
-        answer: {a: "Joey", b: "Rachel", c: "Ross", d:"Pheobe", answer: "Rachel"}
+        question: {q:"Which friends cast member also starred in the film The Leprechaun?"},
+        answer: {a: "Matt Leblanc", b: "Mathew Perry", c: "Jennifer Aniston", d:"Lisa Kudrow", answer: "Jennifer Aniston"}
     }, 
-
 ];
 
 //tracks if game has been started.
@@ -42,11 +41,11 @@ var timesUp;
 //tracks if the user timed out on previous turn.
 var userTimedOut = false;
 
+//Used to hold "this"
 var that;
 
 //used to track if users click occurs during or not during a period where they should select an answer.
 var answerInterval = true;
-
 
 $(document).ready(function(){
 
@@ -60,6 +59,7 @@ $(document).ready(function(){
             newQuestion();
         }   
     });
+
     //registers click of user selection
     $(".selection").on("click", function(){
         that = this;
@@ -84,20 +84,15 @@ $(document).ready(function(){
             setTimeout(newQuestion,5000);
         }
     });
-
-
 });
 
 //starts game and hides "Start Game" text after selecteed.  Only run once in program.
 function startGame (){
-    
     gameStarted = true;
     correct = 0;
     incorrect = 0;
     updateScore();
     $(that).attr("class", "hide");
-
-
 }
 
 //gets question and splices question out of array to prevent same question being asked twice.
@@ -106,7 +101,6 @@ function getQuestion (){
     currentQuestion = QnA[index];
     $("#question-header").text(currentQuestion.question.q);
     QnA.splice(index, 1);
-
 }
 
 //populates answer selections.
@@ -115,8 +109,6 @@ function popAnswers(){
     $(".optionb").text(currentQuestion.answer.b);
     $(".optionc").text(currentQuestion.answer.c);
     $(".optiond").text(currentQuestion.answer.d);
-    
-
 }
 
 //updates scoreboard
@@ -151,13 +143,11 @@ function timedOut () {
 
     //fixed bug of timeout not working before making first selection.
     setTimeout(newQuestion,5000);
-
 }
 
 //used to put timer on each question.
 function startTimer (){
     timesUp = setTimeout(timedOut, 5000);
-
 }
 
 //used to reset timeout and toggle userTimedout to false.
@@ -187,7 +177,6 @@ function userCorrect(){
     correct++;
     $("#question-header").text("You are correct!");
     $(that).addClass("selection-correct");
-
 }
 
 //used when user answers incorrectly
@@ -195,6 +184,5 @@ function userIncorrect(){
     incorrect++;
     $("#question-header").text("Wrong! the correct answer is: " + currentQuestion.answer.answer);
     $(that).addClass("selection-incorrect");
-
 }
 
